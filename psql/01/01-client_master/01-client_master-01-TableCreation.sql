7e83715acd6f2ce7a29eb7946f022884871678c4
Jagadish Sau <jsau5337@gmail.com>
2024-02-18 22:27:41 +0530

    psql Assignment 1

--- psql/01/01-client_master/01-client_master-01-TableCreation.sql
+++ psql/01/01-client_master/01-client_master-01-TableCreation.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS client_master (
-    Client_no VARCHAR(6),
-    Name VARCHAR(30),
+    Client_no VARCHAR(6) PRIMARY KEY CHECK (Client_no LIKE 'C%'),
+    Name VARCHAR(30) NOT NULL,
     Address1 VARCHAR(30),
     Address2 VARCHAR(30),
     City VARCHAR(15),
---
+++ psql/01/05-sales_order_details/05-sales_order_details-01-TableCreation.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS sales_order_details(
+  Order_no VARCHAR(6),
+  Product_no VARCHAR(6),
+  Qty_ordered INT,
+  Qty_disp INT,
+  Product_rate DECIMAL(10,2),
+  CONSTRAINT fk_sales_order FOREIGN KEY (Order_no) REFERENCES sales_order(Order_no),
+  CONSTRAINT fk_product_master FOREIGN KEY (Product_no) REFERENCES product_master(Product_no)
+);
