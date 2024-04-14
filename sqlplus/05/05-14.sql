3d6033e0f6ec8db844a0bd3f6dab3464991a57bb
Subarna Mandal <103353912+SUBARNA-MANDAL@users.noreply.github.com>
2024-04-15 00:42:06 +0530

    unused
    pls remove this unused lines.

--- sqlplus/05/05-14.sql
+++ sqlplus/05/05-14.sql
@@ -7,10 +7,3 @@ WHERE deptno = (
     FROM emp
     WHERE ename LIKE('MILLER')
 );
-
-
-
-
--- If a boy does not get success within 30 years then he need to suicide...
--- Whole world is not ready to accept him...
--- No other choice is there.
