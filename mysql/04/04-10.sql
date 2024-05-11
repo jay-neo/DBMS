280cf59cf3f0e656de3029aaf5618774c1470024
Jagadish Sau <jsau5337@gmail.com>
2024-05-12 01:23:58 +0530

    mysql Assignment 04

--- mysql/04/04-10.sql
+++ mysql/04/04-10.sql
@@ -1,4 +1,9 @@
-SELECT job, AVG(sal) AS avg_salary
-FROM emp
-WHERE job != 'MANAGER'
-GROUP BY job;
+SELECT
+    job,
+    AVG(sal) AS avg_salary
+FROM
+    emp
+WHERE
+    job != 'MANAGER'
+GROUP BY
+    job;
