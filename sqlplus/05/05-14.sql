e1941912dea8be8a1de961fac28def7863268d16
Jagadish Sau <jsau5337@gmail.com>
2024-05-12 01:20:16 +0530

    sqlplus Assignment 05

--- sqlplus/05/05-14.sql
+++ sqlplus/05/05-14.sql
@@ -1,15 +1,17 @@
 -- List the employees belong to department of MILLER.
-
-SELECT ename
-FROM emp
-WHERE deptno = (
-    SELECT deptno
-    FROM emp
-    WHERE ename LIKE('MILLER')
-);
-
-
-
+SELECT
+    ename
+FROM
+    emp
+WHERE
+    deptno = (
+        SELECT
+            deptno
+        FROM
+            emp
+        WHERE
+            ename LIKE ('MILLER')
+    );
 
 -- If a boy does not get success within 30 years then he need to suicide...
 -- Whole world is not ready to accept him...
--- sqlplus/05/05-20.sql
+++ sqlplus/05/05-20.sql
@@ -1,10 +1,30 @@
 --  List all employees who work in DALLAS and 
 -- earn more than any employee working in CHICAGO.
-
-SELECT ename
-FROM emp 
-WHERE deptno = (SELECT deptno FROM dept WHERE loc = 'DALLAS') 
-AND sal > ALL (
-   SELECT sal FROM emp 
-    WHERE deptno = (SELECT deptno FROM dept WHERE loc = 'CHICAGO')
-);
+SELECT
+    ename
+FROM
+    emp
+WHERE
+    deptno = (
+        SELECT
+            deptno
+        FROM
+            dept
+        WHERE
+            loc = 'DALLAS'
+    )
+    AND sal > ALL (
+        SELECT
+            sal
+        FROM
+            emp
+        WHERE
+            deptno = (
+                SELECT
+                    deptno
+                FROM
+                    dept
+                WHERE
+                    loc = 'CHICAGO'
+            )
+    );
